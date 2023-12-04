902ac886cf359e39fc1d195ca1cf2339cfd3079b
wjwong93 <weijunwong93@gmail.com>
2023-12-04 18:44:55 +0900

    Initial commit

---
+++ pgq_read.sql
@@ -0,0 +1,11 @@
+SELECT *
+FROM GRAPH_TABLE(MovieRentals
+	MATCH
+		(cust1 IS Customer)-[e1]->
+		(movie IS Movie)<-[e2](cust2 IS Customer)
+	WHERE e1.Date_Rented > TO_DATE('14-Feb-2022')
+		AND movie.genre='Romantic Comedy'
+		AND e2.Date_Rented > e1.Date_Rented AND cust1.Last_Name != cust2.Last_Name
+	COLUMNS	(cust1.Last_Name AS EarlierRenter, cust2.Last_Name AS LaterRenter, e1.Date_Rented)
+)
+ORDER BY Date_Rented;
---
+++ src/SQLParser.java
@@ -0,0 +1,21 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class SQLParser {
+    public static void main(String[] args) throws Exception {
+        String inputFile = null;
+        if (args.length > 0) inputFile = args[0];
+        InputStream is = System.in;
+        if (inputFile != null) is = new FileInputStream(inputFile);
+        CharStream input = CharStreams.fromStream(is);
+        PostgreSQLLexer lexer = new PostgreSQLLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        PostgreSQLParser parser = new PostgreSQLParser(tokens);
+        ParseTree tree = parser.root();
+        System.out.println(tree.toStringTree(parser));
+
+    }
+}
