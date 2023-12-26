628f184ef93b3fda052af0688b93bcddc7e786ef
wjwong93 <weijunwong93@gmail.com>
2023-12-26 22:28:48 +0900

    test: update yasuda test queries

--- src/test/resources/input/yasuda4.sql
+++ src/test/resources/input/yasuda4.sql
@@ -1,11 +1,11 @@
 SELECT *
-FROM GRAPH_TABLE(T1
+FROM GRAPH_TABLE(DependentNodes
     MATCH
-        (s)-[r IS dep]->+(n),
+        (s)-[r IS DEP]->+(n),
         (m)-[]->{0,1}(n)-[]->{0,1}(l)
-    WHERE s.key =~ '.*estpro1110_202311101200.*'
-        AND n.note = 'true'
-        AND m.note = 'true'
-        AND l.note = 'true'
+    WHERE s.name =~ '.*estpro1110_202311101200.*'
+        AND (m.note = 'TRUE' OR m.note = 'simulation')
+        AND (n.note = 'TRUE' OR n.note = 'simulation')
+        AND (l.note = 'TRUE' OR l.note = 'simulation')
     COLUMNS(s, n, m, l)
 )
